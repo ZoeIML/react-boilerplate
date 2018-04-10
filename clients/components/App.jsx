9fa16aa45f093b0713c37617c0cb617291e2a725
zoeIML <zoe.lisk@gmail.com>
2018-04-10 18:43:48 +1200

    added all until webpack

---
+++ clients/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// funcitonal component - can change to class
+
+const App = () => {
+    return (
+        <h1>Boilerplate working</h1>
+    )
+}
+
+export default App
